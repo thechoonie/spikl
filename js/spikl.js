a19b6348b69c718167061aa096f70a141702bebc
thechoonie <nicola.choon@gmail.com>
2017-03-14 17:35:49 +0000

    Added sign-up and sign-in module
    Tried to add cookies.

--- js/spikl.js
+++ js/spikl.js
@@ -18,3 +18,43 @@ $.fn.isInViewport = function() {
 $(window).on('resize scroll', function(){
   $('.aboutspikl').isInViewport();
 })
+
+// Defining modals
+var modalSignIn
+var modalSignUp
+
+// Get the modal
+$(document).ready(function(){
+  modalSignIn = document.getElementById('id01');
+  modalSignUp = document.getElementById('id02');
+});
+
+
+// When the user clicks anywhere outside of the modal, close it
+$(window).on('click', function(event) {
+    if (event.target == modalSignIn) {
+        modalSignIn.style.display = "none";
+    }
+    if (event.target == modalSignUp) {
+        modalSignUp.style.display = "none";
+    }
+});
+
+function getCookie(){
+  var x = decodeURIComponent(document.cookie);
+  var ca = x.split(';');
+  console.log(ca);
+}
+
+function signIn(){
+    if ($('#email').val()=="nadc500@york.ac.uk" && $('#pw').val()=="Password1"){
+      if ($('.rememberMe').is(':checked')){
+          document.cookie = "username=John Doe";
+      }
+      modalSignIn.style.display = "none";
+      return true;
+    }
+    else {
+      return false;
+    }
+}
